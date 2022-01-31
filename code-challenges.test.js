d9d948b4591d009b862c384aafba7e7515b627c8
Samanthap803b <samanthap803b@gmail.com>
2022-01-31 01:24:14 -0800

    completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -16,32 +16,34 @@
 // Psuedo code: Im going to start my test first im going to describe here im goign to use sentenceCap, after that i use
 // "it" here im going to write wgat i want in return and i want a new array with a sentencew about each person from my previws array with their names capitallized.
 //In my expect im going to wtite the outpit that i want.
-// describe("sentenceCap", () =>{
-// it("returns an array with a sentence about each person with their name capitalized", ()=> {
-//   let people = [
-//     { name: "ford prefect", occupation: "a hitchhiker" },
-//     { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-//     { name: "arthur dent", occupation: "a radio employee" }
-//   ]
-//   expect(sentenceCap(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
-//   })
-// })
+describe("sentenceCap", () =>{
+it("returns an array with a sentence about each person with their name capitalized", () => {
+  let people = [
+    { name: "ford prefect", occupation: "a hitchhiker" },
+    { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+    { name: "arthur dent", occupation: "a radio employee" }
+  ]
+  expect(sentenceCap(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+})
 //RED now i have to make this pass
 // b) Create the function that makes the test pass.
 //First im going to declare my vatiable sentenceCap and create my function
-//I think im going to use map so i can go around my array.
+//I think im going to use map so i can go around my array the same size.
 //I think i have to use .split because i want to separete my array and use toUpperCase so i can have my names capitalized.
 //im going to use .join and for my return im going to use string interpolation so i can return all my output.
+//Im going to split my name so i can make my index 0 . toUpperCase and after that im going to use .join.
 
 
-// const sentenceCap =(array) =>{
-//   return array.map(object => {
-//     let newCase = object.name.split(" ").map(value => value[0].toUpperCase() + value.slice(1)).join(" ")
-//     return `${newCase} is ${object.occupation}`
-//   })
-// }
+const sentenceCap =(array, value) => {
+  return array.map(value => {
+    let newCase = value.name.split(" ").map(value => value[0].toUpperCase() + value.slice(1)).join(" ")
+     return `${newCase} is ${value.occupation}`
+  })
+}
 //So my test fail
-// I had trouble with thoi one i got stuck i ask for help but i still cant make it work :(
+// I had trouble with this one i got stuck i ask for help but i still cant make it work :(
+
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
@@ -50,14 +52,13 @@
 //Im goingn to start describe im going to use remainderDiv, in my "it" im going to write what i want in return.
 //After my expectation im going to write the name on my function and the variable that i want to change at the end i write what i want to see as my outcome.
 
-// describe("remainderDiv", ()=>{
-//   it("mixed data array and returns an array of only the remainder of the numbers when divided by 3",() =>{
-//     expect(remainderDiv(hodgepodge1)).toEqual([ 2, 0, -1, 0 ]),
-//     expect(remainderDiv(hodgepodge2)).toEqual([ 2, 1, 1, ])
-//     let hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
-//     let hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
-//   })
-// })
+describe("remainderDiv", () => {
+  it("mixed data array and returns an array of only the remainder of the numbers when divided by 3",() => {
+    let hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+    let hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+    expect(remainderDiv(hodgepodge1)).toEqual([ 2, 0, -1, 0 ]),
+    expect(remainderDiv(hodgepodge2)).toEqual([ 2, 1, 1, ])
+    })
 //REEEEED :(
 
 
@@ -73,13 +74,14 @@
 // b) Create the function that makes the test pass.
 //I start declaring my variable remainderDiv, I want to make a forloop so I can go in the array and just get
 // what i want that are the numbers after that i want to set my numbers the remainder or my array to be divided by 3.
-// const remainderDiv = (array) => {
-//   let divRemain =[]
-//   for(let i=0; i < array.length; i++){
-//     typeof array[i] === `number` && divRemain.push(array[i]%3)
-//   }
-//       return divRemain
-// }
+const remainderDiv = (array) => {
+  let divRemain = []
+  for(let i=0; i < array.length; i++){
+   if(typeof array[i] === `number` && divRemain.push(array[i]%3))
+  }
+      return divRemain
+}
+
 // My test didnt pass i still dont know where im making the error im going to have to re-do it.
 // Expected output: [ 2, 0, -1, 0 ]
 // Expected output: [ 2, 1, -1 ]
@@ -95,21 +97,22 @@
     expect(cubedArr(cubeAndSum2)).toEqual([1125])
  })
 })
-// // b) Create the function that makes the test pass.
-//Im going to start declaring my variable cubedArr, im going to try a forloop here as well.
-//I want to go throw my array and add al my values after that i want to multiply my total and set a variable to be my answer.
-
+// // // b) Create the function that makes the test pass.
+// //Im going to start declaring my variable cubedArr, im going to try a forloop here as well.
+// //I want to go throw my array and add al my values after that i want to multiply my total and set a variable to be my answer.
+//
 const cubedArr =(array) =>{
   let addCube =0
   for(let i= 0; i < array.length; i++){
     array[i] = array [i]**3 // I try *3*3 first just to try but it didnt work.
     addCube+=array[i]
   }
-  return addCube
+  return addCuben
 }
 
 
 // Today i fail all of them at the begining i thougth it was an easy one i startet with this one but i got stuck again on a syntax error and coudent pass that.
-//I will be returning to fix all my errors. :(
+//I will be returning to fix all my errors. :(, I am still getting that i receive 99 istead of [99] and i try to use .split and after that another method to change from number to string to array and is just not working HELP.
+//I back and im still not able to make this pass I dont know why im having a hard time with this assessment i been stuck here for a while so im going to live it like this.
 // Expected output: 99
 // Expected output: 1125
